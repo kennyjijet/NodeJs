68810791f7774f33dd43811e728d1bec25dbfd0c
kennyjijet <usistal@hotmail.com>
2019-06-17 23:33:22 +1200

    mongo

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
-var http = require("http");
+/*
+var express = require("express");
+var app = express();
 
-//create a server object:
-http
-  .createServer(function(req, res) {
-    res.write("Hello World!"); //write a response to the client
-    res.end(); //end the response
-  })
-  .listen(8080); //the server object listens on port 8080
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
+
+app.get("/url", (req, res, next) => {
+  res.json(["Tony", "Lisa", "Michael", "Ginger", "Food"]);
+});
+*/
+
+const express = require("express");
+const MongoClient = require("mongodb").MongoClient;
+const bodyParser = require("body-parser");
+const app = express();
+const port = 8000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+require("./app/routes")(app, {});
+app.listen(port, () => {
+  console.log("We are live on " + port);
+});
