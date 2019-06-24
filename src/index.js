1bf90166ba0b70eea116010980becb0adfbe46dc
kennyjijet <usistal@hotmail.com>
2019-06-24 13:04:15 +1200

    Node1

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-/*
 var express = require("express");
 var app = express();
 
@@ -9,8 +8,12 @@ app.listen(3000, () => {
 app.get("/url", (req, res, next) => {
   res.json(["Tony", "Lisa", "Michael", "Ginger", "Food"]);
 });
-*/
 
+app.post("/url_post", (req, res, next) => {
+  res.json(["Tony", "Lisa", "Michael", "Ginger", "Food"]);
+});
+
+/*
 const express = require("express");
 const MongoClient = require("mongodb").MongoClient;
 const bodyParser = require("body-parser");
@@ -22,3 +25,4 @@ require("./app/routes")(app, {});
 app.listen(port, () => {
   console.log("We are live on " + port);
 });
+*/
